6fc484cf03f76182f8a89208106a6e8749ceb0e3
CleverDarkSolutions <RotmgADAS@o2.pl>
2023-12-14 13:51:03 +0100

    Infinite scroll working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import InfiniteScroll from "./components/infinite-scroll";
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>Scroll component with a little bit of humor</h1>
+          <div style={{
+              height: '400px',
+              width: '80%',
+              marginLeft: '10%',
+              overflowY: 'scroll'
+          }}>
+              <InfiniteScroll/>
+          </div>
+      </div>
   );
 }
 
---
+++ src/components/infinite-scroll.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export type Joke = {
+    "error": boolean,
+    "category": string,
+    "type": string,
+    "setup": string,
+    "delivery": string,
+    "flags": {
+        "nsfw": boolean,
+        "religious": boolean,
+        "political": boolean,
+        "racist": boolean,
+        "sexist": boolean,
+        "explicit": boolean,
+    },
+    "id": number,
+    "safe": boolean,
+    "lang": string
+}
+
+const InfiniteScroll = () => {
+    const [items, setItems] = useState<Joke[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const [hasMore, setHasMore] = useState(true)
+    const [page,setPage] = useState(0)
+
+    const elementRef = useRef(null)
+
+    const onIntersection = (entries: Array<any>) => {
+        const firstEntry = entries[0]
+        if(firstEntry.isIntersecting && hasMore){
+            fetchMoreItems()
+        }
+    }
+
+    const fetchMoreItems = async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart&amount=5`);
+            const data = await response.json();
+            if(data.jokes.length == 0){
+                setHasMore(false)
+            }
+            else{
+                setItems(prevItems => [...prevItems, ...data.jokes]);
+                setPage(prevPage => prevPage + 1)
+            }
+        } catch (error: any) {
+            setError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+    useEffect(() => {
+        const observer = new IntersectionObserver(onIntersection)
+        if(observer && elementRef.current){
+            observer.observe(elementRef.current)
+        }
+        return () => {
+            if(observer){
+                observer.disconnect()
+            }
+        }
+
+    }, []);
+
+
+    return (
+        <div>
+            {items.map(item => {
+                if(item.setup || item.delivery)return (
+                <div>
+                    <h2>{item.setup}</h2>
+                    <h4>{item.delivery}</h4>
+                </div>
+            )})}
+            {error && <div>{error}</div>}
+            {hasMore && <div ref={elementRef} >Load more items</div>}
+        </div>
+    )
+};
+
+export default InfiniteScroll;
